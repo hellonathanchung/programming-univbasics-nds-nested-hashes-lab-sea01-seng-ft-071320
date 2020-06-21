c2ee9c6fe76faeb17709017143ec709f4048211c
Nathan Chung <chung.nathanh@gmail.com>
2020-06-21 15:54:40 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -22,7 +22,7 @@ literal_aoh = [
   {
     name:  "Don Gately", occupation: "Live-in Staff"}, {
     name:  "Joelle van Dyne", occupation: "Radio Personality" }, {
-      name: "Pat Monteseian", occupation: "None"}, {
+      name: "Pat Monteseian", occupation: "Staff"}, {
       name:  "Kate Gompert", occupation: "None" }, {
       name:  "Bruce Green", occupation: "Fan of Mildred" }
     ]
